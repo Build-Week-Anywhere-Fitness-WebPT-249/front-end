617ca57b88ecfc3e32f35f5ff9282c47ffddcd10
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-01-28 21:00:44 -0500

    Information being added to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,15 @@ import Login from './Login';
 function Home(){
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Welcome to Anywhere Fitness!</h1>
+
+            <h3>Our App</h3>
+            <p>These days, fitness classes can be held anywhere- a park, an unfinished basement or a garage- not just at a traditional gym. Our easy app connects clients and instructors</p> 
+            
+            
+            
+            <p> Certified fitness instructors need an easy way to take the awkwardness out of attendance taking and client payment processing. </p>
+
 
             <div>
                 <Link to='/signUp'>Sign Up</Link>
